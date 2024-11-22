beb57c19b64f45f7147085f95ea53d91b0e2d5e9
Hruthik <mhrithik450@gmail.com>
2024-11-23 00:23:39 +0530

    firstcommit

---
+++ frontend/src/components/Formbuilder.jsx
@@ -0,0 +1,684 @@
+import React, { useEffect, useState } from "react";
+import { styled } from "styled-components";
+import SubForm from "./SubForm";
+
+const FormFields = [
+  {
+    name: "FullName",
+    label: "1. Full Name*",
+    type: "text",
+    placeholder: "Full Name",
+    required: true,
+  },
+  {
+    name: "Designation",
+    label: "3. Designation*",
+    type: "text",
+    placeholder: "Designation",
+    required: true,
+  },
+  {
+    name: "OfficialAddress",
+    label: "4. Official Address*",
+    type: "text",
+    placeholder: "Address",
+    required: true,
+  },
+  {
+    name: "Email",
+    label: "5. Email ID*",
+    type: "email",
+    placeholder: "Email",
+    required: true,
+  },
+  {
+    name: "Phone",
+    label: "6. Contact Number*",
+    type: "number",
+    placeholder: "Phone",
+    required: true,
+    validation: (value) => value.length <= 10,
+  },
+  {
+    name: "AboutYou",
+    label: "7. About You (200-500 words)*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "Objective",
+    label: "8. Objective*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: false,
+  },
+  {
+    name: "Education",
+    label: "9. Education*",
+    type: "subform",
+    fields: [
+      {
+        name: "Course",
+        label: "Course",
+        type: "select",
+        options: ["UG", "PG", "PhD"],
+        required: true,
+      },
+      {
+        name: "University",
+        label: "University Name",
+        type: "text",
+        required: true,
+      },
+      { name: "College", label: "College Name", type: "text", required: true },
+      {
+        name: "PassYear",
+        label: "Year of Passing",
+        type: "number",
+        required: true,
+      },
+      {
+        name: "Percentage",
+        label: "Percentage (%)",
+        type: "number",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "TeachingExperience",
+    label: "10. Teaching Experience*",
+    type: "subform",
+    fields: [
+      {
+        name: "Designation",
+        label: "Designation",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "From",
+        label: "From",
+        type: "date",
+        required: true,
+      },
+      {
+        name: "To",
+        label: "To",
+        type: "date",
+        required: true,
+      },
+      {
+        name: "Department",
+        label: "Dept Name",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "University",
+        label: "University Name",
+        type: "text",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "IndustrialExperience",
+    label: "11. Industrial Experience*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "AcademicContributions",
+    label: "12. Academic Contributions*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "AdministrativePositions",
+    label: "13. Administrative Positions Held*",
+    type: "subform",
+    fields: [
+      {
+        name: "Designation",
+        label: "Designation",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "From",
+        label: "From",
+        type: "date",
+        required: true,
+      },
+      {
+        name: "To",
+        label: "To",
+        type: "date",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Responsibilities",
+    label: "14. State , National and International Level Responsibilities*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "FacultyCouncil",
+    label: "15. Faculty and Academic Council*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "BookAppoinment",
+    label: "16. Board of Appointment*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "BoardStudies",
+    label: "17. Board of Studies*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "BoardExamination",
+    label: "18. Board of Examination*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "ExaminationResponsibilites",
+    label: "19. Examination Responsibilities*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "TechnicalCommitees",
+    label: "20. Technical Commitees*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "Research",
+    label: "21. Research Intersets*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "SponseredProjects",
+    label: "23. Sponsered Research Projects*",
+    type: "subform",
+    fields: [
+      {
+        name: "PIName",
+        label: "PI Name",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "PITitle",
+        label: "Project Title",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "SponserAgency",
+        label: "Sponsering Agency",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "Duration",
+        label: "Duration",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "StartYear",
+        label: "Year of Start",
+        type: "date",
+        required: true,
+      },
+      {
+        name: "EndYear",
+        label: "Year of Completion",
+        type: "date",
+        required: true,
+      },
+      {
+        name: "Amount",
+        label: "Sanctioned Amount",
+        type: "number",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "ConsultancyProjects",
+    label: "24. Consultancy Projects*",
+    type: "subform",
+    fields: [
+      {
+        name: "PIName",
+        label: "PI Name",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "PITitle",
+        label: "Project Title",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "SponserAgency",
+        label: "Sponsering Agency",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "Duration",
+        label: "Duration",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "StartYear",
+        label: "Year of Start",
+        type: "date",
+        required: true,
+      },
+      {
+        name: "EndYear",
+        label: "Year of Completion",
+        type: "date",
+        required: true,
+      },
+      {
+        name: "Amount",
+        label: "Sanctioned Amount",
+        type: "number",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Patents",
+    label: "25. Patents*",
+    type: "subform",
+    fields: [
+      {
+        name: "Author",
+        label: "Author",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "PTitle",
+        label: "Patent Title",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "PName",
+        label: "Patent Name",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "DesignNo",
+        label: "Design No",
+        type: "number",
+        required: true,
+      },
+      {
+        name: "Year",
+        label: "Year of Grant",
+        type: "date",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Books",
+    label: "26. Books*",
+    type: "subform",
+    fields: [
+      {
+        name: "Author",
+        label: "Author",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "BTitle",
+        label: "Title of the Book",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "PubName",
+        label: "Publisher Name",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "Pages",
+        label: "Pages",
+        type: "number",
+        required: true,
+      },
+      {
+        name: "Date",
+        label: "Date",
+        type: "date",
+        required: true,
+      },
+      {
+        name: "ISBN",
+        label: "ISBN",
+        type: "number",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "AwardsAndRecognition",
+    label: "27. Awards And Recognition*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "PhD",
+    label: "28. Ph.D Guidance (Awards and Ongoing)",
+    type: "subform",
+    fields: [
+      {
+        name: "ScholarName",
+        label: "Scholar Name",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "TTitle",
+        label: "Thesis Title",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "University",
+        label: "University Name",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "Year",
+        label: "Year",
+        type: "date",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "NotableAchivements",
+    label: "29. Notable Achivements*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "InvitedTalks",
+    label: "30. Invited Talks",
+    type: "subform",
+    fields: [
+      {
+        name: "Topic",
+        label: "Topic",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "Organization",
+        label: "Organization",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "Year",
+        label: "Year",
+        type: "date",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Workshops",
+    label: "31. Workshops/Seminars/Conferences Organized*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "Memberships",
+    label: "32. Memberships in Professional Bodies",
+    type: "subform",
+    fields: [
+      {
+        name: "Organization",
+        label: "Organization",
+        type: "text",
+        required: true,
+      },
+      {
+        name: "Title",
+        label: "Title of Membership",
+        type: "text",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "AbroadVisits",
+    label: "33. Abroad Visits*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "SocialActivities",
+    label: "34. Social and Other Activities (Details)*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+  {
+    name: "Courses",
+    label: "35. List of Courses Taught*",
+    type: "textarea",
+    placeholder: "Enter your description...",
+    required: true,
+  },
+];
+
+const Formbuilder = () => {
+  const [formState, setformState] = useState({});
+
+  const HandleChange = (key, value) => {
+    setformState((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
+  const handleChange = (e, field) => {
+    const Value = e.target.value;
+    if (field?.validation && !field.validation(Value)) {
+      return;
+    }
+
+    const { name, value } = e.target;
+    HandleChange(name, value);
+  };
+
+  const handleInput = (e) => {
+    const textarea = e.target;
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
+  return (
+    <Wrapper>
+      <Form onSubmit={handleSubmit}>
+        <Hero>
+          <H1>Faculty Curriculum Vitae</H1>
+          <P>
+            for UVCE Website<A href="/www.uvce.ac.in">www.uvce.ac.in</A>
+          </P>
+          {FormFields.map((field, index) => {
+            if (field.type === "textarea") {
+              return (
+                <div key={index}>
+                  <Label>{field.label}</Label>
+                  <Desc
+                    placeholder={field.placeholder}
+                    onInput={handleInput}
+                    onChange={(e) => handleChange(e, field)}
+                    name={field.name}
+                    spellCheck="true"
+                    required={field.required}
+                  />
+                </div>
+              );
+            }
+
+            if (field.type === "subform") {
+              return (
+                <div key={index}>
+                  <Label>{field.label}</Label>
+                  <SubForm
+                    formFields={field.fields}
+                    onchange={(data) => HandleChange(field.name, data)}
+                  />
+                </div>
+              );
+            }
+
+            return (
+              <div key={index}>
+                <Label>{field.label}</Label>
+                <Input
+                  type={field.type}
+                  name={field.name}
+                  placeholder={field.placeholder}
+                  value={(formState && formState[field.name]) || ""}
+                  onChange={(e) => handleChange(e, field)}
+                  required={field.required}
+                />
+              </div>
+            );
+          })}
+          <Button type="submit">Submit</Button>
+        </Hero>
+      </Form>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  max-height: max-content;
+  width: 100%;
+`;
+
+const Form = styled.form`
+  box-shadow: 0px 0px 2px 2px rgba(0, 0, 0, 0.3);
+  scrollbar-width: none;
+  border-radius: 10px;
+  overflow-y: auto;
+  height: 100vh;
+  max-width: 40%;
+  margin: auto;
+  padding: 2rem;
+
+  @media (max-width: 450px) {
+    max-width: 90%;
+    padding: 1.2rem;
+  }
+`;
+
+const Hero = styled.div`
+  max-height: max-content;
+`;
+
+const H1 = styled.h1`
+  max-height: max-content;
+`;
+
+const Label = styled.label`
+  max-height: max-content;
+`;
+
+const P = styled.p`
+  max-height: max-content;
+  display: flex;
+  column-gap: 0.5rem;
+`;
+
+const A = styled.a`
+  max-height: max-content;
+`;
+
+const Desc = styled.textarea`
+  width: 90%;
+  border: none;
+  outline: none;
+  display: block;
+  resize: none;
+  margin: 0.5rem 0 1rem 1rem;
+  border-bottom: 2px solid black;
+`;
+
+const Input = styled.input`
+  width: 90%;
+  border: none;
+  outline: none;
+  display: block;
+  margin: 1rem 0 1.5rem 1rem;
+  border-bottom: 2px solid black;
+
+  &::-webkit-inner-spin-button,
+  &::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
+
+const Button = styled.button`
+  background-color: cyan;
+  max-height: max-content;
+  border-radius: 10px;
+  font-weight: 700;
+  margin: 1rem 0;
+  width: 100%;
+`;
+
+export default Formbuilder;
---
+++ frontend/src/components/SubForm.jsx
@@ -0,0 +1,215 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const SubForm = ({ formFields, onchange }) => {
+  const [entries, setEntries] = useState([]);
+  const [SubFormData, setSubFormData] = useState({});
+  const [alert, setalert] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const isValid = formFields.every((field) =>
+      field.required ? SubFormData[field.name] : true
+    );
+
+    if (!isValid) {
+      setalert({ type: "danger", msg: "Please fill all the details" });
+      setTimeout(() => {
+        setalert(null);
+      }, 1500);
+      return;
+    }
+
+    setEntries((prev) => [...prev, SubFormData]);
+    setSubFormData({});
+    setalert({ type: "success", msg: "Successfully Added" });
+    setTimeout(() => {
+      setalert(null);
+    }, 1500);
+  };
+
+  useEffect(() => {
+    onchange(entries);
+  }, [entries]);
+
+  const handleDelete = (index) => {
+    setEntries((prev) => prev.filter((_, i) => i != index));
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSubFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <>
+      {entries &&
+        entries.length > 0 &&
+        entries.map((entry, index) => (
+          <Preview key={index}>
+            {formFields.map((field, index) => (
+              <PrevSubInputBox key={index}>
+                <Strong>{field.label}:</Strong>
+                <SubLabel>{entry[field.name]}</SubLabel>
+              </PrevSubInputBox>
+            ))}
+            <DelSubButtonBox>
+              <DelSubButton onClick={() => handleDelete(index)}>
+                Delete
+              </DelSubButton>
+            </DelSubButtonBox>
+          </Preview>
+        ))}
+
+      <SubData>
+        {formFields.map((field, index) => (
+          <SubInputBox key={field.name}>
+            <SubLabel htmlFor={field.name}>{field.label}:</SubLabel>
+            {field.type === "select" ? (
+              <Select
+                id={field.name}
+                name={field.name}
+                value={(SubFormData && SubFormData[field.name]) || ""}
+                onChange={handleChange}
+              >
+                <Option value="">Select {field.label}</Option>
+                {field.options.map((option, index) => (
+                  <Option key={index} value={option}>
+                    {option}
+                  </Option>
+                ))}
+              </Select>
+            ) : field.type === "date" ? (
+              <SubInput
+                type="date"
+                name={field.name}
+                value={(SubFormData && SubFormData[field.name]) || ""}
+                onChange={handleChange}
+                max={new Date().toISOString().split("T")[0]}
+              />
+            ) : (
+              <SubInput
+                id={field.name}
+                type={field.type}
+                name={field.name}
+                value={(SubFormData && SubFormData[field.name]) || ""}
+                onChange={handleChange}
+              ></SubInput>
+            )}
+          </SubInputBox>
+        ))}
+
+        {alert && (
+          <div
+            className={`alert alert-${alert.type}`}
+            role="alert"
+            style={{ marginInline: "1rem" }}
+          >
+            {alert.msg}
+          </div>
+        )}
+
+        <SubButtonBox>
+          <SubButton onClick={(e) => handleSubmit(e)}>Add+</SubButton>
+        </SubButtonBox>
+      </SubData>
+    </>
+  );
+};
+
+const SubData = styled.div`
+  box-shadow: 0px 0px 2px 2px rgba(0, 0, 0, 0.3);
+  margin: 0.5rem 0 1rem 1rem;
+  max-height: max-content;
+  border-radius: 10px;
+  max-width: 90%;
+`;
+
+const SubInputBox = styled.div`
+  max-height: max-content;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  column-gap: 0.5rem;
+  align-items: center;
+
+  @media (max-width: 450px) {
+    margin-top: 0.5rem;
+  }
+`;
+
+const PrevSubInputBox = styled(SubInputBox)`
+  grid-template-columns: 1fr 2fr;
+`;
+
+const Select = styled.select`
+  margin: 1rem 0 1rem 1rem;
+  height: 25px;
+  border-radius: 40px;
+  padding-left: 0.5rem;
+  max-width: 80%;
+`;
+
+const Option = styled.option`
+  max-height: max-content;
+`;
+
+const SubInput = styled.input`
+  width: 90%;
+  border: none;
+  outline: none;
+  display: block;
+  margin: 1rem 0 1.5rem 1rem;
+  border-bottom: 2px solid black;
+
+  &::-webkit-inner-spin-button,
+  &::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  max-width: 80%;
+  width: 100%;
+`;
+
+const SubLabel = styled.label`
+  max-height: max-content;
+  margin-left: 1.5rem;
+`;
+
+const Strong = styled.strong`
+  max-height: max-content;
+  margin-left: 1.5rem;
+`;
+
+const SubButtonBox = styled.div`
+  max-height: max-content;
+  display: flex;
+  justify-content: flex-end;
+  width: 90%;
+`;
+
+const DelSubButtonBox = styled(SubButtonBox)``;
+
+const SubButton = styled.button`
+  max-height: max-content;
+  background-color: #63c5da;
+  padding: 0.5rem 1.5rem;
+  margin-bottom: 1rem;
+  border-radius: 10px;
+  font-weight: 700;
+`;
+
+const DelSubButton = styled(SubButton)`
+  background-color: red;
+  color: white;
+  margin-bottom: 0.5rem;
+`;
+
+const Preview = styled(SubData)`
+  padding: 1rem 0rem;
+`;
+
+export default SubForm;
