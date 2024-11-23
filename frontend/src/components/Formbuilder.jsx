8a9baee6e13f96422b4debeeaed410ce5a6d6710
Hruthik <mhrithik450@gmail.com>
2024-11-23 10:48:30 +0530

    thirdcommit

--- frontend/src/components/Formbuilder.jsx
+++ frontend/src/components/Formbuilder.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import { collection, addDoc } from "firebase/firestore";
-import { db } from "../firebase";
 import SubForm from "./SubForm";
+import { db } from "../firebase";
 
 const FormFields = [
   {
